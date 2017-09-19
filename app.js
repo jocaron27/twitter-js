da591b095d1ba27c438d5913429518ead601e4a6
Jeanne Castillo <jcaron27@gmail.com>
2017-09-19 14:43:02 -0400

    Create server, templates and router

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
+const volleyball = require('volleyball');
+app.use(volleyball);
+const morgan = require('morgan');
+morgan('tiny');
+const nunjucks = require('nunjucks');
 const http = require('http');
 const server = http.createServer();
 const PORT = 3000;
-const volleyball = require('volleyball');
-var morgan = require('morgan');
+const tweetBank = require('./tweetBank');
+const routes = require('./routes');
 
-app.use(volleyball);
 
-morgan('tiny');
 
-app.use(function(req, res, next) {
-    console.log(req.method + " " + req.url + " " + res.statusCode);
-    if (req.url.split('/')[1] === ('special')) {
-        console.log('you reached the special area.');
-    }
-    next();
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+app.use('/', routes);
+
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.url} ${res.statusCode}`);
+  next();
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
-
-app.get('/news', function (req, res) {
-    res.send('Hello World! : NEWS')
-})
-
-app.get('/special', function (req, res) {
-    res.send('Hello World! : SPECIAL')
-})
-
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-  })
+  console.log('Server listening on port 3000!');
+});
+
+
 
 
 
