57745c97b5b02e8850670815bd94c653727df1a2
Jeanne Castillo <jcaron27@gmail.com>
2017-09-19 10:49:29 -0400

    add middleware

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require( 'express' );
 const app = express();
-
-
 const http = require('http');
-
 const server = http.createServer();
-
 const PORT = 3000;
+const volleyball = require('volleyball');
+var morgan = require('morgan');
 
-// app.use(function(req, res, next) {
-//     console.log(req.url);
-//     next();
-//   });
+app.use(volleyball);
+
+morgan('tiny');
+
+app.use(function(req, res, next) {
+    console.log(req.method + " " + req.url + " " + res.statusCode);
+    if (req.url.split('/')[1] === ('special')) {
+        console.log('you reached the special area.');
+    }
+    next();
+});
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
@@ -19,6 +24,10 @@ app.get('/', function (req, res) {
 
 app.get('/news', function (req, res) {
     res.send('Hello World! : NEWS')
+})
+
+app.get('/special', function (req, res) {
+    res.send('Hello World! : SPECIAL')
 })
 
 app.listen(3000, function () {
